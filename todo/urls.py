cb9a96d181279e9518d53d065a75044543557f10
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-02 20:08:04 -0800

    renders todos from db to index/ url

--- todo/urls.py
+++ todo/urls.py
@@ -19,5 +19,5 @@ from todos.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', index)
+    path('index/', index)
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+# import the Todo model from the .models module
+from .models import Todo
 
 def index(request):
-    return render(request, 'index.html')
+    # create a variable that conatins all todos from the db
+    todos = Todo.objects.all()
+    
+    # first argument is the request object
+    # second argument is the view file defined in the template directory
+    # thrid argument is the data to be passed to the template view
+    return render(request, 'index.html', {'todos': todos})
